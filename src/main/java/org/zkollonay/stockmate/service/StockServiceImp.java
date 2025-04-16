71a8c4564db21c271a5f80def02443520a88fc69
Kollonay Zoltán <zoltan@kollonay.hu>
2025-04-16 13:44:04 +0200

    feat: added javadoc to StockServiceImp.java

--- src/main/java/org/zkollonay/stockmate/service/StockServiceImp.java
+++ src/main/java/org/zkollonay/stockmate/service/StockServiceImp.java
@@ -22,6 +22,13 @@ public class StockServiceImp implements StockService {
   private final StockRepository stockRepository;
   private final StockMapper stockMapper;
 
+
+  /**
+   * Adds a new stock to the system.
+   *
+   * @param fullStockDTO The DTO containing the details of the new stock.
+   * @return The DTO representing the newly added stock.
+   */
   @Override
   public FullStockDTO addNewStock(FullStockDTO fullStockDTO) {
     Stock stock = stockMapper.toEntity(fullStockDTO);
@@ -29,23 +36,50 @@ public class StockServiceImp implements StockService {
     return stockMapper.toNewDTO(stock);
   }
 
+
+  /**
+   * Retrieves all stocks with pagination.
+   *
+   * @param pageable The pagination information.
+   * @return A page of FullStockDTO objects representing the stocks.
+   */
   @Override
   public Page<FullStockDTO> getAllStocks(Pageable pageable) {
     Page<Stock> stockPage = stockRepository.findAll(pageable);
     return stockPage.map(stockMapper::toNewDTO);
   }
 
+
+  /**
+   * Retrieves a summary of all stocks.
+   *
+   * @return A list of StockDTO objects representing the summary of stocks.
+   */
   @Override
   public List<StockDTO> getSumStocks() {
     return stockRepository.getStockSummary();
   }
 
+
+  /**
+   * Retrieves all stocks with the given stock identifier.
+   *
+   * @param stockIdentifier The identifier of the stock.
+   * @return A list of FullStockDTO objects with the specified identifier.
+   */
   @Override
   public List<FullStockDTO> getStockByStocksIdentifier(String stockIdentifier) {
     return stockRepository.findByStockIdentifier(stockIdentifier).stream().map(stockMapper::toNewDTO).toList();
   }
 
 
+  /**
+   * Retrieves a summary of a specific stock based on its identifier.
+   *
+   * @param stockIdentifier The identifier of the stock.
+   * @return A StockDTO representing the summary of the stock.
+   * @throws EntityNotFoundException If no stock is found with the given identifier.
+   */
   @Override
   public StockDTO getSumStockByStocksIdentifier(String stockIdentifier) {
     Optional<StockDTO> stockDTO = stockRepository.getStockSummaryByStockIdentifier(stockIdentifier);
@@ -53,11 +87,26 @@ public class StockServiceImp implements StockService {
     return stockDTO.orElseThrow(() -> new EntityNotFoundException(stockIdentifier));
   }
 
+
+  /**
+   * Deletes a stock by its ID.
+   *
+   * @param stockID The ID of the stock to delete.
+   */
   @Override
   public void deleteStockById(long stockID) {
     stockRepository.deleteById(stockID);
   }
 
+
+  /**
+   * Updates an existing stock by its ID.
+   *
+   * @param fullStockDTO The DTO containing the updated stock details.
+   * @param stockID      The ID of the stock to update.
+   * @return The DTO representing the updated stock.
+   * @throws EntityNotFoundException If no stock is found with the given ID.
+   */
   @Override
   public FullStockDTO updateStockById(FullStockDTO fullStockDTO, long stockID) {
     Stock oldStock = stockRepository.findById(stockID)
@@ -68,16 +117,39 @@ public class StockServiceImp implements StockService {
     return stockMapper.toNewDTO(oldStock);
   }
 
+
+  /**
+   * Filters stocks based on a given filter string in name, summary description, or stock identifier.
+   *
+   * @param filter The filter string.
+   * @return A list of StockDTO objects that match the filter.
+   */
   @Override
   public List<StockDTO> filterStocks(String filter) {
     return stockRepository.findFilteredStocks(filter).stream().map(stockMapper::toDTO).toList();
   }
 
+
+  /**
+   * Retrieves stocks purchased within a specific date range.
+   *
+   * @param from The starting date of the range (inclusive).
+   * @param to   The ending date of the range (inclusive).
+   * @return A list of FullStockDTO objects purchased within the given period.
+   */
   @Override
   public List<FullStockDTO> getStocksByYearFromTo(LocalDateTime from, LocalDateTime to) {
     return stockRepository.findByPurchaseDateBetween(from, to).stream().map(stockMapper::toNewDTO).toList();
   }
 
+
+  /**
+   * Retrieves the full description of a stock based on its identifier.
+   *
+   * @param stockIdentifier The identifier of the stock.
+   * @return The full description of the stock.
+   * @throws EntityNotFoundException If no full description is found for the given stock identifier.
+   */
   @Override
   public String getFullDescriptionByStocksIdentifier(String stockIdentifier) {
     Optional<String> description = stockRepository.getFullDescription(stockIdentifier);
@@ -85,6 +157,13 @@ public class StockServiceImp implements StockService {
     return description.orElseThrow(() -> new EntityNotFoundException("Full description not found for stock identifier: " + stockIdentifier));
   }
 
+
+  /**
+   * Retrieves stocks purchased in a specific year.
+   *
+   * @param year The year of purchase.
+   * @return A list of FullStockDTO objects purchased in the specified year.
+   */
   @Override
   public List<FullStockDTO> getStocksByYear(Integer year) {
     return stockRepository.findByPurchaseYear(year).stream().map(stockMapper::toNewDTO).toList();
