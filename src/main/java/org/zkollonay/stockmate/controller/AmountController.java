37273b981d952c56eb05203b954767c2b849fc13
Kollonay Zoltán <zoltan@kollonay.hu>
2025-04-16 13:45:28 +0200

    feat: added javadoc to AmountController.java

--- src/main/java/org/zkollonay/stockmate/controller/AmountController.java
+++ src/main/java/org/zkollonay/stockmate/controller/AmountController.java
@@ -17,15 +17,27 @@ public class AmountController {
 
   private final AmountService amountService;
 
+
+  /**
+   * Retrieves the total investment amount for a specific currency.
+   *
+   * @param currency The currency to calculate the total investment for.
+   * @return ResponseEntity containing the InvestedDTO with the total investment amount.
+   */
   @GetMapping("/total-investment-currency")
   public ResponseEntity<InvestedDTO> getTotalInvestmentAmountByCurrency(@RequestParam Currency currency) {
     return ResponseEntity.ok(amountService.getTotalInvestmentAmountByCurrency(currency));
   }
 
+
+  /**
+   * Retrieves the total investment amount across all currencies, converted to HUF.
+   *
+   * @return ResponseEntity containing the InvestedDTO with the total investment amount in HUF.
+   */
   @GetMapping("/total-investment")
   public ResponseEntity<InvestedDTO> getTotalInvestmentAmount() {
     return ResponseEntity.ok(amountService.getTotalInvestmentAmount());
   }
-
 
 }
